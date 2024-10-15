20bab96cc67b586b4a8ab251c99f132c977ad4ba
Jake Bathman <jake.bathman@gmail.com>
2024-10-15 16:40:02 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $config = config('maps');
+    $hash = trim(exec('git log --pretty="%h" -n1 HEAD'));
     return view('welcome', [
         'maps' => $config['maps'],
         'filters' => $config['filters'],
         'games' => $config['games'],
+        'commitHash' => $hash,
     ]);
 });
