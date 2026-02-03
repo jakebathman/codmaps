7d0868cb4104adab9b6643215e8f478b6fc7585f
Jake Bathman <jake.bathman@gmail.com>
2026-02-02 19:56:27 -0600

    Capitalize decode input

--- resources/views/livewire/decode.blade.php
+++ resources/views/livewire/decode.blade.php
@@ -20,7 +20,7 @@
                     name="codeInput"
                     placeholder="A01-ABC12-XYZ89-1"
                     wire:model.live="codeInput"
-                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                    class="uppercase block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
                 />
             </div>
         </div>
